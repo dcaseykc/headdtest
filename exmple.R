1c98e5e0e5c698bae3f41f22be3ac547df985da1
Daniel Casey <dcasey@kingcounty.gov>
2021-09-14 15:05:13 -0700

    fix exmple

--- exmple.R
+++ exmple.R
@@ -1,17 +1,6 @@
 library('data.table')
 library('headtest')
 
-blarg <- function(...){
-  r = data.table::data.table(...)
-  data.table::setattr(r, 'class', c('blarg', class(r)))
-  r
-}
-
-"[.blarg" <- function(x, i, j, by, ...){
-  mc <- match.call()
-  mc[[1]] <- quote(data.table:::`[.data.table`)
-  res = eval.parent(mc)
-}
 a = data.table(1:10)
 b = blarg(1:10)
 
